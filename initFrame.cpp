775e88cc2b0268061fc6e40d4b4f3243e64210a2
gmlwns0704 <84502133+gmlwns0704@users.noreply.github.com>
2022-11-08 18:17:06 +0900

    autoBackup

---
+++ initFrame.cpp
@@ -0,0 +1,65 @@
+﻿// initFrame.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "GitProjectManager.h"
+#include "afxdialogex.h"
+#include "initFrame.h"
+
+CString initFileManager::gitURL;
+CString initFileManager::defaultBackupURL;
+CString initFileManager::autoBackupMessage;
+
+
+// initFrame 대화 상자
+
+IMPLEMENT_DYNAMIC(initFrame, CDialogEx)
+
+initFrame::initFrame(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_INIT, pParent)
+{
+}
+
+initFrame::~initFrame()
+{
+}
+
+void initFrame::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+
+	//기본값 불러와서 에딧컨트롤에 넣어놓기
+	SetDlgItemText(IDS_EDIT_GIT_URL, initFileManager::gitURL);
+	SetDlgItemText(IDS_EDIT_REPO_URL, initFileManager::defaultBackupURL);
+	SetDlgItemText(IDS_EDIT_AUTO_BACKUP_MSG, initFileManager::autoBackupMessage);
+}
+
+
+BEGIN_MESSAGE_MAP(initFrame, CDialogEx)
+	ON_BN_CLICKED(IDS_OK, &initFrame::OnBnClickedOk)
+	ON_BN_CLICKED(IDS_CANCEL, &initFrame::OnBnClickedCancel)
+END_MESSAGE_MAP()
+
+
+// initFrame 메시지 처리기
+
+
+void initFrame::OnBnClickedOk()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	GetDlgItemTextW(IDS_EDIT_GIT_URL, initFileManager::gitURL);
+	GetDlgItemTextW(IDS_EDIT_REPO_URL, initFileManager::defaultBackupURL);
+	GetDlgItemTextW(IDS_EDIT_AUTO_BACKUP_MSG, initFileManager::autoBackupMessage);
+
+	initFileManager::saveInit();
+
+	DestroyWindow();
+}
+
+
+void initFrame::OnBnClickedCancel()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	DestroyWindow();
+}
---
+++ newUploaderFrame.cpp
@@ -0,0 +1,111 @@
+﻿// newUploaderFrame.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "GitProjectManager.h"
+#include "afxdialogex.h"
+#include "newUploaderFrame.h"
+#include "projEditorFrame.h"
+
+
+// newUploaderFrame 대화 상자
+
+IMPLEMENT_DYNAMIC(newUploaderFrame, CDialogEx)
+
+newUploaderFrame::newUploaderFrame(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_NEW_UPLOADER, pParent)
+{
+
+}
+
+newUploaderFrame::~newUploaderFrame()
+{
+}
+
+void newUploaderFrame::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(newUploaderFrame, CDialogEx)
+	ON_BN_CLICKED(IDOK, &newUploaderFrame::OnBnClickedOk)
+	ON_BN_CLICKED(IDC_BUTTON_PROJ_DIR_FIND, &newUploaderFrame::OnBnClickedButtonProjDirFind)
+END_MESSAGE_MAP()
+
+
+// newUploaderFrame 메시지 처리기
+
+
+void newUploaderFrame::OnBnClickedOk()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CString projName, branch, projDir, gitURL;
+	GetDlgItemTextW(IDC_EDIT_PROJNAME, projName);
+	GetDlgItemTextW(IDC_EDIT_BRANCH, branch);
+	GetDlgItemTextW(IDC_EDIT_PROJDIR, projDir);
+	GetDlgItemTextW(IDC_EDIT_GIT_URL, gitURL);
+	if (projName == "") {
+		MessageBox(_T("프로젝트 이름을 입력하시오"), _T("warning"), MB_ICONWARNING);
+		return;
+	}
+	if (branch== "") {
+		MessageBox(_T("프로젝트 브랜치를 입력하시오"), _T("warning"), MB_ICONWARNING);
+		return;
+	}
+	if (projDir == "") {
+		MessageBox(_T("프로젝트 경로를 입력하시오"), _T("warning"), MB_ICONWARNING);
+		return;
+	}
+	if (GitUploader::getProj((string)CT2CA(projName))) {
+		MessageBox(_T("해당 이름의 프로젝트가 이미 존재합니다"), _T("warning"), MB_ICONWARNING);
+		return;
+	}
+	GitUploader* newObj =  new GitUploader(projDir, projName, _T(""), gitURL);
+
+	CDialogEx::OnOK();
+
+	DestroyWindow();
+
+	projEditorFrame* nUFrame = new projEditorFrame(newObj);
+	nUFrame->Create(IDD_EDIT_UPLOADER);
+	nUFrame->ShowWindow(SW_SHOW);
+	nUFrame->UpdateWindow();
+}
+
+
+BOOL newUploaderFrame::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+	GetDlgItem(IDC_EDIT_BRANCH)->SetWindowTextW(_T("main"));
+	GetDlgItem(IDC_EDIT_GIT_URL)->SetWindowTextW(initFileManager::defaultBackupURL);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+
+void newUploaderFrame::OnBnClickedButtonProjDirFind()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	//출처 : https://psmkr0225.tistory.com/192
+	BROWSEINFO BrInfo;
+	TCHAR szBuffer[512]; // 경로저장 버퍼 
+
+	::ZeroMemory(&BrInfo, sizeof(BROWSEINFO));
+	::ZeroMemory(szBuffer, 512);
+
+	BrInfo.hwndOwner = GetSafeHwnd();
+	BrInfo.lpszTitle = _T("프로젝트 폴더 선택");
+	BrInfo.ulFlags = BIF_NEWDIALOGSTYLE | BIF_EDITBOX | BIF_RETURNONLYFSDIRS;
+	LPITEMIDLIST pItemIdList = ::SHBrowseForFolder(&BrInfo);
+	::SHGetPathFromIDList(pItemIdList, szBuffer); // 파일경로 읽어오기
+
+	// 경로를 가져와 사용할 경우, Edit Control 에 값 저장
+	CString str;
+	str.Format(_T("%s"), szBuffer);
+	SetDlgItemText(IDC_EDIT_PROJDIR, str);
+}
